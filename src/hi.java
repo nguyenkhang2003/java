bd0cb1e7c66c6839a1182a77c95efcd1ac7d777c
nguyenkhang2003 <khangln.21it@vku.udn.vn>
2023-06-05 20:16:09 +0700

    update hi.java

--- src/hi.java
+++ src/hi.java
@@ -1,10 +1,15 @@
 public class hi {
+
     public static void main(String[] args) {
-        
-    System.out.println("Hello");
-        
+        String name;
+        float height;
+        int age;
+        name = "Chau Kter";
+        height = 1.7f;
+        age = 21;
+        System.out.println(name);
+        System.out.println(height);
+        System.out.println(age);
     }
-     
-        
-    
+
 }
